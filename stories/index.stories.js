2c418a84b92bf216ec3f69e76c467315227ac04c
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2019-11-11 13:20:34 +0100

    chore(stories): fix linting

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,13 +1,13 @@
 export default {
-  title: 'Demo',
-};
+  title: 'Demo'
+}
 
-export const heading = () => '<h1>Hello World</h1>';
+export const heading = () => '<h1>Hello World</h1>'
 
 export const button = () => {
-  const btn = document.createElement('button');
-  btn.type = 'button';
-  btn.innerText = 'Hello Button';
-  btn.addEventListener('click', e => console.log(e));
-  return btn;
-};
+  const btn = document.createElement('button')
+  btn.type = 'button'
+  btn.innerText = 'Hello Button'
+  btn.addEventListener('click', e => console.log(e))
+  return btn
+}
